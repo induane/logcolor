8a6b21178b844ebe42e257c803b3fdf7fe325827
Brant Watson <brant.watson@propylon.com>
2017-01-06 15:32:35 -0600

    Add licensing info

--- setup.py
+++ setup.py
@@ -7,7 +7,12 @@ INSTALL_REQUIRES = []
 
 setup(
     name="logcolor",
+    author="Brant Watson",
+    author_email="oldspiceap@gmail.com",
+    description="Simple log formatters for colored output",
     version="1.0.0",
+    home_page="https://github.com/induane/logcolor",
+    licence="wtfpl",
     packages=find_packages('src'),
     package_dir={"": "src"},
     install_requires=INSTALL_REQUIRES,
